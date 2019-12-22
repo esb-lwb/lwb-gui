98407a2928054c5c1615566509b79f9f4444ccab
br <Burkhardt.Renz@mni.thm.de>
2019-12-22 16:56:37 +0100

    uses lwb 2.1.3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [rsyntaxtextarea/rsyntaxtextarea "3.0.5z"]
-                 [lwb "2.1.2"]]
+                 [lwb "2.1.3"]]
   :main lwb-gui.main
   :profiles {:uberjar  {:aot :all}}
   :uberjar-name "lwb-gui.jar")

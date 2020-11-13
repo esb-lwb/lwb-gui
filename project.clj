0eff69a7558855c044c3a8351d073fb8809bc8a2
br <Burkhardt.Renz@mni.thm.de>
2020-11-13 13:14:15 +0100

    lwb 2.2.0

--- project.clj
+++ project.clj
@@ -1,18 +1,22 @@
-(defproject lwb-gui "1.0.0-BETA"
-  :date "2020-01-15"
+(defproject lwb-gui "1.0.0"
+  :date "2020-11-15"
   :description "A simple GUI for the Logic Workbench"
   :url "https://guthub.com/esb-lwb/lwb-gui"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  ; for seesaw
                  [com.miglayout/miglayout "3.7.4"]
-                 [com.jgoodies/forms "1.2.1"]
+                 [com.jgoodies/forms "1.3.0"]
                  [org.swinglabs.swingx/swingx-core "1.6.3"]
                  [j18n "1.0.2"]
                  [rsyntaxtextarea/rsyntaxtextarea "3.0.9-esb-dev"]
                  [seesaw "1.5.1-esb-dev"]
-                 [lwb "2.1.5"]] ;; update consts/about too!
+                 [lwb/lwb "2.2.0"]] ;; update consts/about too!
   :main esb.dev.lwb-gui.main
   :profiles {:uberjar  {:aot :all}}
   :uberjar-name "lwb-gui.jar")
+
+; lwb is not available as a maven jar
+; compile github/esb-lwb/lwb and put the lwb.jar into your local maven repo
+; lein localrepo install lwb.jar lwb n.n.n
